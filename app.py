2dd50fb5408eee2db031101822c60e482dc80b9e
Luolivers <89819274+Luolivers@users.noreply.github.com>
2021-11-28 21:36:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
